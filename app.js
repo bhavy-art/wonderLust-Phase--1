7f630c32d6ac2eafeca7a73162a808ed381a88c0
bhavy-art <bhavybatra085@gmail.com>
2026-06-19 16:04:23 +0530

    Validation schema using Joi

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js");
 
 main()
   .then(() => {
@@ -26,6 +27,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
+
+const validateListing = (req, res, next) => {
+  let {error} = listingSchema.validate(req.body);
+  if(error){
+    let errMsg = error.details.map((el)=>el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  }else{
+    next();
+  }
+};
 
 // Home Route
 app.get("/", (req, res) => {
@@ -52,10 +63,15 @@ app.get("/listings/:id", wrapAsync( async (req, res) => {
 }));
 
 // Create Route
-app.post("/listings", wrapAsync( async (req, res, next) => {
+app.post("/listings", validateListing, wrapAsync( async (req, res, next) => {
   // try{
-    if(!req.body.listing){
-      throw new ExpressError(400,"Send valid data for listing");
+    // if(!req.body.listing){
+    //   throw new ExpressError(400,"Send valid data for listing");
+    // }
+    let result = listingSchema.validate(req.body);
+    console.log(result);
+    if(result.error){
+      throw new ExpressError(404,result.error);
     }
     const newListing = new Listing(req.body.listing);
     await newListing.save();
@@ -76,7 +92,7 @@ app.get("/listings/:id/edit", wrapAsync(async (req,res)=>{
 
 
 // update route
-app.put("/listings/:id", wrapAsync(async (req,res)=>{
+app.put("/listings/:id", validateListing, wrapAsync(async (req,res)=>{
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listings/${id}`);
