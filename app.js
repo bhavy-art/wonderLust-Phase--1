87a0d9952c043037117fb5ebc3e4c77f11828898
bhavy-art <bhavybatra085@gmail.com>
2026-07-07 18:03:23 +0530

    mongodb atlas

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
+const dbUrl = process.env.ATLASDB_URL;
 
 main()
   .then(() => {
@@ -34,7 +35,7 @@ main()
   });
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/wonderLust");
+  await mongoose.connect(dbUrl);
 }
 
 app.set("view engine", "ejs");
